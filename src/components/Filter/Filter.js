ae55c30570cf5f1e026e3dbaa751dcbd9c9c73f0
alinakyrianova <alina.kyrianova@gmail.com>
2023-08-28 18:12:54 +0200

    task 3.1 done

---
+++ src/components/Filter/Filter.js
@@ -0,0 +1,16 @@
+
+import { InputFilter } from './Filter.styled';
+
+export const Filter = ({ filter, getContact }) => {
+  return (
+    <div>
+      <InputFilter
+        value={filter}
+        onChange={getContact}
+        placeholder="Search..."
+        type="text"
+        name="filter"
+      />
+    </div>
+  );
+};
